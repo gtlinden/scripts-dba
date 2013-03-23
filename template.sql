13d5cef0c5f89676512896867c75a3ba2809b888
Terry van der Linden <terryvanderlinden@gmail.com>
2013-03-23 22:59:29 +0100

    template document for sql files

---
+++ template.sql
@@ -0,0 +1,10 @@
+-- File name:   {name}.sql
+-- Purpose:       
+--                                                                                                                
+-- Usage:       @{name}.sql
+--------------------------------------------------------------------------------
+PROMPT 
+PROMPT 
+PROMPT rkbs and wrkbs read/wrote bytes per second
+PROMPT rtkbs and wtkbs , Total : except to controlfile I/O
+
